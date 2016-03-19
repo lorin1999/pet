7894f6c55a33f5aaa5abf7ca9ed165126b6d4d4c
Oliver Bestwalter <oliver.bestwalter@avira.com>
2016-03-19 11:05:12 +0100

    dead code

--- privacy.py
+++ privacy.py
@@ -8,12 +8,3 @@ About levels of privacy in Python:
 * Attributes starting and ending with two underscores are part of internal
   mechanics and protocols
 """
-def contains_undefined_name():
-    adrfse
-
-print "after undefined name"  # Will this be executed?
-
-def i_contain_broken_syntax():
-    definitely broken syntax :)
-
-print "after broken syntax"  # and this?
