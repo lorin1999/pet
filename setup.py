b486ba8042acec1eec2bf4fb02cc5c10982f2d63
Oliver Bestwalter <oliver.bestwalter@avira.com>
2016-03-22 18:39:03 +0100

    bump version (dev)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-course',
-    version='0.1.0',
+    version='0.2.0-dev',
     description='Materials and tools for an introductory Python Course',
     url='https://github.com/obestwalter/python-course',
     author='Oliver Bestwalter',
