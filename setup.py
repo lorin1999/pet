cf741df12bc4cbceed73e43e5f88687df97ec3ec
Oliver Bestwalter <oliver.bestwalter@avira.com>
2016-03-28 01:13:52 +0200

    deps

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+"""
+pip install https://github.com/ipython-contrib/
+IPython-notebook-extensions/archive/master.zip --user
+"""
 import logging
-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('setup.py')
@@ -10,16 +13,18 @@ log.debug("found packages: %s", packages)
 
 setup(
     name='python-course',
-    version='0.2.0-dev',
+    version='0.2.0.dev0',
     description='Materials and tools for an introductory Python Course',
     url='https://github.com/obestwalter/python-course',
     author='Oliver Bestwalter',
     license='MIT',
     install_requires=[
-        'ipython',
-        'jupyter',
-        'plumbum',
         'requests[security]',
+        'plumbum',
+
+        'jupyter',
+        'nbpresent',
+        'funcsigs',  # undocumented nbpresent dependency
     ],
     packages=packages,
     entry_points={'console_scripts': ['pc-make = _tools.make:main']},
